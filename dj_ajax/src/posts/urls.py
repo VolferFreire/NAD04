c26b55c03ee83f6318225e545146754c55283faf
VolferFreire <volfer.freire@gmail.com>
2025-04-01 18:37:24 -0400

    Finished th First Ajax Call part

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import (
     post_line_and_create,
+    
+    hello_world_view
 )
 
 app_name = 'posts'
 urlpatterns = [
     path('', post_line_and_create, name='main-board'),
+    
+    path('hello-world/', hello_world_view, name='hello-world'),
 ]
