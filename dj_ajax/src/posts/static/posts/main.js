67d84136b190f2c3fbbd858fbcf9ff475cf5cfd2
VolferFreire <volfer.freire@gmail.com>
2025-04-07 19:01:16 -0400

    Finished resetting the form afoter adding new posts

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -164,6 +164,7 @@ postForm.addEventListener("submit", (e) => {
       likeUnlikePosts();
       $("#addPostModal").modal("hide");
       handleAlerts("success", "New Post Added!");
+      postForm.reset();
     },
     error: function (error) {
       console.log(error);
