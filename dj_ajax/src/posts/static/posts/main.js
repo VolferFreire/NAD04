0e5e4eaf3ddc375d5e19c6910046eb10bc97dbbf
VolferFreire <volfer.freire@gmail.com>
2025-04-07 15:23:17 -0400

    Finished part 2 of like button with ajax

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,9 +1,57 @@
-console.log("Hello world");
+console.log("hello world");
 
 const postsBox = document.getElementById("posts-box");
 const spinnerBox = document.getElementById("spinner-box");
 const loadBtn = document.getElementById("load-btn");
 const endBox = document.getElementById("end-box");
+
+const getCookie = (name) => {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === `${name}=`) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+};
+
+const csrftoken = getCookie("csrftoken");
+
+const likeUnlikePosts = () => {
+  const likeUnlikeForms = [
+    ...document.getElementsByClassName("like-unlike-forms"),
+  ];
+  likeUnlikeForms.forEach((form) =>
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const clickedId = e.target.getAttribute("data-form-id");
+      const clickedBtn = document.getElementById(`like-unlike-${clickedId}`);
+
+      $.ajax({
+        type: "POST",
+        url: "/like-unlike/",
+        data: {
+          csrfmiddlewaretoken: csrftoken,
+          pk: clickedId,
+        },
+        success: function (response) {
+          console.log(response);
+          clickedBtn.textContent = response.liked ? `Unlike (${response.count})` : `Like (${response.count})`
+          // clickedBtn.classList.toggle("btn-primary");
+        },
+        error: function (error) {
+          console.log(error);
+        },
+      });
+    })
+  );
+};
 
 let visible = 3;
 
@@ -19,30 +67,33 @@ const getData = () => {
         console.log(data);
         data.forEach((el) => {
           postsBox.innerHTML += `
-            <div class="card mb-2" >
-              <div class="card-body">
-                <h5 class="card-title">${el.title}</h5>
-                <p class="card-text">${el.body}</p>
-              </div>
-              <div class="card-footer">
-                  <div class="row">
-                    <div class="col-1">
-                      <a href="#" class="btn btn-primary"> Details</a>
-                    </div>
-                    <div class="col-1">
-                      <a href="#" class="btn btn-primary">
-                        ${
-                          el.liked
-                            ? `Unlike (${el.count})`
-                            : `Like (${el.count})`
-                        }
-                      </a>
-                    </div>
-                  </div>
-              </div>  
-            </div>
-            `;
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">${el.title}</h5>
+                                <p class="card-text">${el.body}</p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <a href="#" class="btn btn-primary">Details</a>
+                                    </div>
+                                    <div class="col-2">
+                                        <form class="like-unlike-forms" data-form-id="${
+                                          el.id
+                                        }">
+                                            <button href="#" class="btn btn-primary" id="like-unlike-${
+                                              el.id
+                                            }">${
+            el.liked ? `Unlike (${el.count})` : `Like (${el.count})`
+          }</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `;
         });
+        likeUnlikePosts();
       }, 100);
       console.log(response.size);
       if (response.size === 0) {
