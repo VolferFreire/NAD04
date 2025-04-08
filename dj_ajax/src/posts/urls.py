eeea405c96139b59c14d7ce7b468f50714717de2
VolferFreire <volfer.freire@gmail.com>
2025-04-07 20:13:25 -0400

    Finished post detail page part 2

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -4,6 +4,7 @@ from .views import (
     load_post_data_view,
     like_unlike_post,
     post_detail,
+    post_detail_data_view,
     
     hello_world_view
 )
@@ -12,7 +13,8 @@ app_name = 'posts'
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
-    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('<pk>/', post_detail, name='post-detail'),
     
+    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),    
 ]
